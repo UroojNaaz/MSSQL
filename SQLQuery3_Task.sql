506b13e2fecb0bcbd7b0a5dfe87a3c66c2d1ce4d
UROOJ NAZ <142103317+UroojNaaz@users.noreply.github.com>
2024-08-13 13:20:14 +0500

    Update SQLQuery3_Task.sql

--- SQLQuery3_Task.sql
+++ SQLQuery3_Task.sql
@@ -1,56 +1,69 @@
-----------------------------------------MSSQL QUERIES TASK---------------------------------------
-use Hr;
+------------------------------------------------------MSSQL QUERIES TASK------------------------------------------------------
+use Hr
+ 
+--MSSQL QUERIE:
+ 
+--QUERY-01:write a  SQL query to find the unique designations of the employees. Return job name.
 
---//QUERY-01:write a  SQL query to find the unique designations of the employees. Return job name.
- select distinct job_id
+ select distinct job_id as "job_name"
  from employees;
 
- --//QUERY-02:write a SQL query to list the employees Details, increased their salary by 15%.)
+--QUERY-02:write a SQL query to list the employees Details, increased their salary by 15%.
+
  select *,
- salary* 1.15 as "salary_inc"
+ salary * 1.15 as "salary_inc"
  from employees;
  
- --//QUERY-03:Write a query in SQL to produce the output of employees as follows.
- select *
- from employees;
+--QUERY-03:Write a query in SQL to produce the output of employees as follows.
 
- --//QUERY-04:write a  SQL query to identify those employees whose salaries exceed 3000 after receiving a 25% salary increase. Return complete information about the employees.
+ select * from employees;
+
+--QUERY-04:write a  SQL query to identify those employees whose salaries exceed 3000 after receiving a 25% salary increase. Return complete information about the employees.
+
  select *
  from employees
  where (salary * 1.25)> 3000;
 
+--QUERY-05:write a SQL query to find the names of the employees whose length is six. Return employee name.
 
- --//QUERY-05:write a SQL query to find the names of the employees whose length is six. Return employee name.
  select *
  from employees
- where len (first_name)>6;
+ where len (first_name)=6;
 
- --//QUERY-06:write a SQL query to find out which employees joined in the month of January. Return complete information about the employees.
+ QUERY-06:write a SQL query to find out which employees joined in the month of January. Return complete information about the employees.
+
  select *
  from employees
  where MONTH(hire_date)=1;
 
- --//QUERY-07:write a SQL query to find those employees whose salaries are less than 3500. Return complete information about the employees.
+--QUERY-07:write a SQL query to find those employees whose salaries are less than 3500. Return complete information about the employees.
+
  select *
  from employees
  where salary < 3500;
 
- --//QUERY-08:write a SQL query to identify those employees who joined the company in 1991. Return complete information about the employees.
+--QUERY-08:write a SQL query to identify those employees who joined the company in 1991. Return complete information about the employees.
  select *
  from employees
- where YEAR(hire_date)=1999;
+ where YEAR(hire_date)=1991;
 
- --//QUERY-09:write a SQL query to find those employees who joined before 1st April 1991. Return employee ID, employee name, hire date and salary.
+--QUERY-09:write a SQL query to find those employees who joined before 1st April 1991. Return employee ID, employee name, hire date and salary.
  select *
  from employees
- where hire_date<'1991-01-01';
+ where hire_date<'1991-04-01';
 
- --//QUERY-10:write a  SQL query to find out which employees earn more than 100 per day as a salary. Return employee ID, employee name, salary, and experience.
- select *
- from employees
- where salary/31 >= 100;
+--QUERY-10:write a  SQL query to find out which employees earn more than 100 per day as a salary. Return employee ID, employee name, salary, and experience.
 
- --//QUERY-11:write a SQL query to identify the employees whose salaries are odd. Return complete information about the employees.
+ select employee_id,
+ first_name +' '+ last_name as "employee_name", 
+ salary,
+ job_id as "experience"
+ from employees
+ where salary/30 >= 100;
+
+ --QUERY-11:write a SQL query to identify the employees whose salaries are odd. Return complete information about the employees.
+
  select *
  from employees
- where salary/2 >=1;
+ where salary%2 >=1;
+
