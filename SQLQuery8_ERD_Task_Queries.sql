0b97eca3a6509f171c7f033f56993f28553e7f01
UROOJ NAZ <142103317+UroojNaaz@users.noreply.github.com>
2024-08-19 16:14:47 +0500

    ERD Task's Queries

---
+++ SQLQuery8_ERD_Task_Queries.sql
@@ -0,0 +1,14 @@
+-----------------------------------------------------------ERD Task's Queries---------------------------------------------------------------------
+
+--Use Database:
+USE Mart;
+
+-- Select all data from tables:
+SELECT * FROM Productline;
+SELECT * FROM Office;
+SELECT * FROM Employee;
+SELECT * FROM Customer;
+SELECT * FROM Payment;
+SELECT * FROM Order_Table;
+SELECT * FROM Order_Product;
+SELECT * FROM Product;
