252b5cfb64a67949dc0be7d67e4da4718793545d
UROOJ NAZ <142103317+UroojNaaz@users.noreply.github.com>
2024-08-22 17:19:26 +0500

    PROCEDURES,TRIGGER AND TRIGGER LOG

---
+++ SQLQuery13.sql
@@ -0,0 +1,75 @@
+------------------------------------------------------PROCEDURES----------------------------------------------------------
+
+-- USE DATABASE:
+USE HR;
+
+-- EXECUTE TABLE:
+SELECT * FROM REGIONS;
+
+--*********************************************PROCEDURES FOR REGION TABLE************************************************
+
+-- CREATE INSERT PROCEDURE
+CREATE PROC INSERTREGION @ID INT, @NAME VARCHAR(30)
+AS 
+BEGIN
+    INSERT INTO REGIONS (REGION_ID, REGION_NAME)
+    VALUES (@ID, @NAME);
+END;
+GO
+
+-- DROP PROCEDURE (IF NEEDED):
+-- DROP PROC INSERTREGION;
+
+-- ADD VALUES BY USING PROCEDURE
+EXEC INSERTREGION @ID = 5, @NAME = 'MIDDLE EAST';
+EXEC INSERTREGION @ID = 6, @NAME = 'AFRICA';
+EXEC INSERTREGION @ID = 7, @NAME = 'MIDDLE ASIA';
+EXEC INSERTREGION @ID = 8, @NAME = 'MIDDLE AMERICA';
+
+-- EXECUTE TABLE TO SEE THE INSERTED VALUES:
+SELECT * FROM REGIONS;
+
+-- CREATE DELETE PROCEDURE
+CREATE PROC DELETEREGION @R_ID INT
+AS 
+BEGIN
+    DELETE FROM REGIONS 
+    WHERE REGION_ID = @R_ID;
+END;
+GO
+
+-- DELETE VALUES BY USING PROCEDURE
+EXEC DELETEREGION @R_ID = 8;
+
+-- EXECUTE TABLE TO CONFIRM DELETION:
+SELECT * FROM REGIONS;
+
+-- CREATE UPDATE PROCEDURE
+CREATE PROC UPDATEREGION @R_ID INT, @NAME VARCHAR(30)
+AS 
+BEGIN
+    UPDATE REGIONS
+    SET REGION_NAME = @NAME
+    WHERE REGION_ID = @R_ID;
+END;
+GO
+
+-- UPDATE VALUES BY USING PROCEDURE
+EXEC UPDATEREGION @R_ID = 8, @NAME = 'AMERICA';
+
+-- EXECUTE TABLE TO SEE THE UPDATED VALUES:
+SELECT * FROM REGIONS;
+
+---------------------------------------------------------TRIGGER-------------------------------------------------------------
+-- EXECUTE TABLE:
+SELECT * FROM REGIONS;
+
+--************************************************TRIGGER FOR REGION TABLE**************************************************
+
+
+
+-------------------------------------------------------TRIGGER-LOG-------------------------------------------------------------
+-- EXECUTE TABLE:
+SELECT * FROM REGIONS;
+
+--*********************************************TRIGGER-LOG FOR REGION TABLE**************************************************
